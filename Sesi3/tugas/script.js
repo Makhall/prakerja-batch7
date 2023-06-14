8ef3a0b8b2808b2dc57954dbbd1ab6bde2d01722
Makhall <siddiqmakhalli17@gmail.com>
2023-06-14 11:53:55 +0700

    change dir

--- Sesi3/tugas/script.js
+++ Sesi3/tugas/script.js
@@ -7,8 +7,4 @@ menuToggle.addEventListener('change', function() {
   } else {
     menu.style.display = 'none';
   }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 584dece4f2459ea0ef5d0a443884b6abaeb89753
